3ee47bb316d696a6306ec4e6f8e3b4784c2d2f4b
KrzysMak1 <krzysiekmak11@gmail.com>
2026-01-19 11:40:56 +0100

    Use NPC UUID and block set armor changes

--- work/dream-kowal-108jar-a3274186/plugin/src/java/cc/dreamcode/kowal/controller/ArmorEquipController.java
+++ work/dream-kowal-108jar-a3274186/plugin/src/java/cc/dreamcode/kowal/controller/ArmorEquipController.java
@@ -5,20 +5,34 @@ import eu.okaeri.injector.annotation.Inject;
 import org.bukkit.event.EventHandler;
 import com.destroystokyo.paper.event.player.PlayerArmorChangeEvent;
 import cc.dreamcode.kowal.ParticleCache;
+import cc.dreamcode.kowal.config.PluginConfig;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
 
 public class ArmorEquipController implements Listener
 {
     private final ParticleCache particleCache;
+    private final PluginConfig pluginConfig;
     
-    @EventHandler
+    @EventHandler(ignoreCancelled = true)
     public void onArmorEquip(final PlayerArmorChangeEvent event) {
+        if (this.isSetItem(event.getOldItem()) || this.isSetItem(event.getNewItem())) {
+            event.setCancelled(true);
+            return;
+        }
         this.particleCache.check(event.getPlayer());
+    }
+
+    private boolean isSetItem(final ItemStack item) {
+        return item != null
+                && this.pluginConfig.kowalItems != null
+                && this.pluginConfig.kowalItems.containsKey(item.getType());
     }
     
     @Inject
     @Generated
-    public ArmorEquipController(final ParticleCache particleCache) {
+    public ArmorEquipController(final ParticleCache particleCache, final PluginConfig pluginConfig) {
         this.particleCache = particleCache;
+        this.pluginConfig = pluginConfig;
     }
 }
